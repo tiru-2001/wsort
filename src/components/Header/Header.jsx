8afb71015d05fed265896e33494522575e4ba01a
tiru-2001 <tirumaleshae709@gmail.com>
2024-08-16 18:05:57 +0530

    imported link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import './header.scss';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import { navLinks } from '../../utilities';
 import { NavLink } from 'react-router-dom';
